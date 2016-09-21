20afba5bb1ee03647f38e5c4c90c900fa1801e9f
CaiqueMitsuoka <caique_mitsuoka@hotmail.com>
2016-09-21 11:21:14 -0300

    Add survivor class

---
+++ survivor.py
@@ -0,0 +1,60 @@
+# -*- coding: <UTF-8> -*-
+import json
+class Survivor:
+    _id = None
+    name = ''
+    age = 0
+    gender = ''
+    lastLocation = {'x':.0,'y':.0}
+    inventory = {}
+    infectionReports = 0
+
+    def __init__(self, _id , name, age, gender, lat, lon, invent, infectRep):
+        self._id = _id
+        self.name = str(name)
+        self.age = int(age)
+        self.gender = str(gender)
+        self.lastLocation['x'] = float(lat)
+        self.lastLocation['y'] = float(lon)
+        self.inventory = invent
+        self.infectionReports = infectRep
+
+
+    def canTrade(self):
+        if self.infectionReports < 3:
+            return True
+        return False
+
+    def jsonToSuvivor(self,s):
+        survivorDic = json.loads(s)
+        return Survivor(
+            survivorDic['id'],
+            survivorDic['name'],
+            survivorDic['age'],
+            survivorDic['gender'],
+            survivorDic['lastLocation']['x'],
+            survivorDic['lastLocation']['y'],
+            survivorDic['inventory'],
+            survivorDic['infectionReports']
+        )
+
+    def survivorToDic(self):
+        jso = {"id":self._id
+         , "path": '/survivor/' + str(self._id)
+         , "name": self.name
+         , "age": self.age
+         , "gender": self.gender
+         , "infectionReports": self.infectionReports
+         , "lastLocation": {
+             "x": self.lastLocation['x']
+             ,"y": self.lastLocation['y']
+         }
+         , "inventory": {
+             "water": self.inventory['water']
+             ,"food": self.inventory['food']
+             ,"medication":self.inventory['medication']
+             ,"ammunition": self.inventory['ammunition']
+         }
+        }
+
+        return jso
