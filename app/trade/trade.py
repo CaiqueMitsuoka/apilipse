072486b08476b8ceaa5c92d687d85036135c60db
CaiqueMitsuoka <caique_mitsuoka@hotmail.com>
2016-10-10 22:09:38 -0300

    Fix trade bug

--- app/trade/trade.py
+++ app/trade/trade.py
@@ -11,7 +11,7 @@ class TradeManager:
         trade_left_json = trade_json['trade'][0]
         trade_right_json = trade_json['trade'][1]
         self.items_left = Inventory(trade_left_json['itens'])
-        self.itens_right = Inventory(trade_right_json['itens'])
+        self.items_right = Inventory(trade_right_json['itens'])
         db = SurvivorDb('survivors')
         self.traderLeft = db.searchById(trade_left_json['id'])
         self.traderRight = db.searchById(trade_right_json['id'])
@@ -22,7 +22,7 @@ class TradeManager:
         return (items.water * points.water) + (items.food * points.food) + (items.medication * points.medication) + (items.ammunition * points.ammunition)
 
     def equalPoints(self):
-        if self.calcPoints(self.items_left) == self.calcPoints(self.itens_right):
+        if self.calcPoints(self.items_left) == self.calcPoints(self.items_right):
             return True
         return False
 
@@ -31,10 +31,10 @@ class TradeManager:
 
             if self.equalPoints() and self.traderLeft.canTrade() and self.traderRight.canTrade():
 
-                if self.traderLeft.canGiveItens(self.items_left) and self.traderRight.canGiveItens(self.itens_right):
+                if self.traderLeft.canGiveItens(self.items_left) and self.traderRight.canGiveItens(self.items_right):
 
-                    new_inventory_left = self.traderLeft.performTrade(self.items_left,self.itens_right)
-                    new_inventory_right = self.traderRight.performTrade(self.items_left,self.items_left)
+                    new_inventory_left = self.traderLeft.performTrade(self.items_left, self.items_right)
+                    new_inventory_right = self.traderRight.performTrade(self.items_right,self.items_left)
 
                     db = SurvivorDb('survivors')
                     db.updateById( self.traderLeft._id, 'inventory', new_inventory_left)
@@ -54,7 +54,7 @@ class TradeManager:
                     json = {'error':'survivor is giving more itens then he can'}
                     return jsonify(json), 409
             else:
-                json = {'error':'trade is only possible with same quantity of points'}
+                json = {'error':'trade is only possible with same quantity of points and survivors isn\'t infected'}
                 return jsonify(json), 409
         else:
             json = {'error':'survivor not found'}
